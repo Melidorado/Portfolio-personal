0ef45eb17f09fe94c7f354a9fae36941eac3378e
Melidorado <meliidorado@hotmail.com>
2020-09-21 16:04:28 -0300

    menu desplegable en responsive funcionando

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const botonDespleglable = document.getElementById("hamburguesa")
-const barraNavegacion = document.getElementById("barraNavegacion")
-const menuDesplegable = document.getElementById("desplegable")
+const barraNavegacion = document.getElementById("barra-principal")
+const menuDesplegable = document.getElementById("barraNavegacion")
+
+const secciones = document.getElementsByClassName("li-nav")
+
 
 
 botonDespleglable.onclick = () => {
 
-    console.log("hello")
-    menuDesplegable.classList.toggle(`mostrar-menu`)
+    menuDesplegable.classList.toggle(`show`)
     barraNavegacion.classList.toggle(`cambio-desplegable`)
-    botonDespleglable.classList.toggle(`menus`)
+
+    if (menuDesplegable.classList.contains(`show`)) {
+        botonDespleglable.setAttribute(`class`, `fa fa-times`)
+    }
+    else {
+        botonDespleglable.setAttribute(`class`, `fas fa-bars`)
+    }
+
+    for (let seccion of secciones) {
+
+        seccion.onclick = ()=> {
+            menuDesplegable.classList.remove(`show`)
+            barraNavegacion.classList.remove(`cambio-desplegable`)
+            botonDespleglable.setAttribute(`class`, `fas fa-bars`)
+        } 
+        
+    }
     
 }
 
