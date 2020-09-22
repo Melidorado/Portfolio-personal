3e0745194cad8c51efc92d8b67d0ead4551383a0
Melidorado <meliidorado@hotmail.com>
2020-09-22 10:08:52 -0300

    animaciones y formulario agregado

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const botonDespleglable = document.getElementById("hamburguesa")
+// -------------------  MENÚ DESPLEGABLE  ----------------------//
+
+const botonDesplegable = document.getElementById("hamburguesa")
 const barraNavegacion = document.getElementById("barra-principal")
 const menuDesplegable = document.getElementById("barraNavegacion")
 
@@ -6,16 +8,16 @@ const secciones = document.getElementsByClassName("li-nav")
 
 
 
-botonDespleglable.onclick = () => {
+botonDesplegable.onclick = () => {
 
     menuDesplegable.classList.toggle(`show`)
     barraNavegacion.classList.toggle(`cambio-desplegable`)
 
     if (menuDesplegable.classList.contains(`show`)) {
-        botonDespleglable.setAttribute(`class`, `fa fa-times`)
+        botonDesplegable.setAttribute(`class`, `fa fa-times`)
     }
     else {
-        botonDespleglable.setAttribute(`class`, `fas fa-bars`)
+        botonDesplegable.setAttribute(`class`, `fas fa-bars`)
     }
 
     for (let seccion of secciones) {
@@ -23,12 +25,65 @@ botonDespleglable.onclick = () => {
         seccion.onclick = ()=> {
             menuDesplegable.classList.remove(`show`)
             barraNavegacion.classList.remove(`cambio-desplegable`)
-            botonDespleglable.setAttribute(`class`, `fas fa-bars`)
+            botonDesplegable.setAttribute(`class`, `fas fa-bars`)
         } 
         
     }
     
 }
 
+//----------------------------  FILTRO EN PROYECTOS POR USO DE TECNOLOGIA  ------------------------------//
+
+const botones = document.getElementsByClassName("filtros")
+const proyectos = document.getElementsByClassName("proyecto")
+console.log(botones)
+console.log(proyectos)
 
 
+for (let boton of botones) {
+    boton.onclick = () => {
+        
+        for (let proyecto of proyectos) {
+            proyecto.classList.add("hidden")     
+             if (boton.dataset.nombre === proyecto.dataset.nombre) {
+                 proyecto.classList.remove("hidden")
+                 
+             }
+             else if (boton.dataset.nombre === "todos") {
+                 proyecto.classList.remove("hidden")
+             }
+             else {
+                 proyecto.classList.add("hidden")
+             }
+         
+         } 
+        
+    }
+}
+
+/* filtrarPorTecnologia = () => {
+    for (let proyecto of proyectos) {
+       proyecto.classList.add("hidden")
+       boton.classList.add()     
+        if (boton.dataset.nombre === proyecto.dataset.nombre) {
+            proyecto.classList.remove("hidden")
+            
+        }
+        else if (boton.dataset.nombre === "todos") {
+            proyecto.classList.remove("hidden")
+        }
+        else {
+            proyecto.classList.add("hidden")
+        }
+    
+    }
+}
+
+cambiosBotones = () => {
+    if (boton.classList.contains("boton"))
+}
+
+
+
+
+ */
